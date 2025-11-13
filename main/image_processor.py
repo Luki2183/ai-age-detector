1b8245741f4206f3719dac1aaf6d79325189707f
Luki2183 <lenternus@gmail.com>
2025-11-13 19:05:02 +0100

    fixed bug
    img variable could be none

--- main/image_processor.py
+++ main/image_processor.py
@@ -5,10 +5,12 @@ def preprocess_image_or_frame(data, target_size=(200, 200), train_data=False):
     # Sprawdza potrzebe wczytania pliku
     if isinstance(data, str):
         img = cv2.imread(data)
+    else:
+        img = data
 
     # Zmiana na skale szarości, tylko do trenowania
     if train_data:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY)
 
     # Zmiana rozmiaru
     img = cv2.resize(img, target_size)
