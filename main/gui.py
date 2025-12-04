27e436d53e4fbac2e6a853a41cc6cbbd49d15a37
Luki2183 <lenternus@gmail.com>
2025-12-04 14:05:13 +0100

    improved gui
    face detection, age prediction, auto rectangle on face(s)

--- main/gui.py
+++ main/gui.py
@@ -12,29 +12,53 @@ camera_loop_id = None
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+
+def assign_age_and_convert_frame(frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+        # Required value assignment associated with UnboundLocalError
+        # do NOT delete
+        ages_list = []
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            age = -1
+            age = age_prediction.predict_age(model, frame)
+            ages_list.append(int(age))
+            label = "Age: " + str(int(age))
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame, label, (x, y - 10), font, 0.8, (255, 0, 0), 2)
+        return Image.fromarray(frame), ages_list
+
+
+def convert_ages_list_to_str(ages_list: list):
+    length_of_ages_list = len(ages_list)
+    if length_of_ages_list < 1:
+        return f'Age: -1'
+    elif length_of_ages_list == 1:
+        return f'Age: {ages_list.pop()}'
+    else:
+        return f'Ages: {str(ages_list)[1:-1:]}'
+
+
 def open_camera():
     def update_frame():
         global current_img, camera_loop_id
         ret, frame = cam.read()
         
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            age = age_prediction.predict_age(model, frame)
-            label = "Wiek: " + str(int(age))
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(frame, label, (x, y - 10), font, 0.8, (255, 0, 0), 2)
+        img, ages_list = assign_age_and_convert_frame(frame)
         
         # Lower fps
         time.sleep(0.1)
 
-        current_img = Image.fromarray(frame)
+        current_img = img
+        current_age = convert_ages_list_to_str(ages_list)
+        age_panel.config(text=current_age)
+        age_panel.text = current_age
         resize_image()
-        camera_loop_id = panel.after(10, update_frame)
-        
+        camera_loop_id = img_panel.after(10, update_frame)
+
     release_cam()
 
     global cam
@@ -46,7 +70,7 @@ def open_camera():
 
 
 def open_picture():
-    global current_img
+    global current_img, age_panel
 
     release_cam()
 
@@ -55,20 +79,30 @@ def open_picture():
     )
     if not filepath:
         return
-    current_img = Image.open(filepath)
+    
+    frame = cv2.imread(filepath)
+
+    img, ages_list = assign_age_and_convert_frame(frame)
+
+    current_img = img
+    current_age = convert_ages_list_to_str(ages_list)
+    age_panel.config(text=current_age)
+    age_panel.text = current_age
     root.update_idletasks()
     resize_image()
+
 
 def release_cam():
     global cam, camera_loop_id
     if camera_loop_id:
-        panel.after_cancel(camera_loop_id)
+        img_panel.after_cancel(camera_loop_id)
         camera_loop_id = None
     
     try:
         cam.release()
     except:
         pass
+
 
 root = tk.Tk()
 root.title("Age prediction")
@@ -81,11 +115,11 @@ current_img = None
 def resize_image(event=None):
     if current_img is None:
         return
-    w, h = panel.winfo_width(), panel.winfo_height()
+    w, h = img_panel.winfo_width(), img_panel.winfo_height()
     resized = current_img.resize((max(1, w-50), max(1, h-50)))
     img_tk = ImageTk.PhotoImage(resized)
-    panel.config(image=img_tk)
-    panel.image = img_tk
+    img_panel.config(image=img_tk)
+    img_panel.image = img_tk
 
 
 btn_cam = tk.Button(root, text="Open Camera", command=open_camera)
@@ -96,8 +130,12 @@ btn_pic = tk.Button(root, text="Open Picture", command=open_picture)
 btn_pic.pack(pady=10)
 
 
-panel = tk.Label(root)
-panel.pack(expand=True, fill="both")
+age_panel = tk.Label(root, text = 'Age: ')
+age_panel.pack(pady=10)
+
+
+img_panel = tk.Label(root)
+img_panel.pack(expand=True, fill="both")
 
 
 root.bind("<Configure>", resize_image)
